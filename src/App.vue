33ce19b4408d6b13f011398422dc9fb68c1abf1f
Francesca Rödl <francesca.roedl@verdigado.com>
2026-04-01 17:50:13 +0200

    set up basic editor from documentation

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
   This is gonna be an editor
+  <tip-tap/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TipTap from "@/components/TipTap.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TipTap,
   }
 }
 </script>
